bd50b9d1b7415790eee23d154d71b617a1945d34
Alicia Saci <lyciiasci@gmail.com>
2023-10-23 18:56:57 +0200

    entity user and etablissement

---
+++ server/src/Entity/Etablissement.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\EtablissementRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtablissementRepository::class)]
+#[ApiResource]
+class Etablissement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $kbis = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $validation = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $jours_ouverture = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $horraires_ouverture = null;
+
+    #[ORM\OneToMany(mappedBy: 'etablissement', targetEntity: User::class)]
+    private Collection $etablissement;
+
+    public function __construct()
+    {
+        $this->etablissement = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getKbis(): ?string
+    {
+        return $this->kbis;
+    }
+
+    public function setKbis(?string $kbis): static
+    {
+        $this->kbis = $kbis;
+
+        return $this;
+    }
+
+    public function isValidation(): ?bool
+    {
+        return $this->validation;
+    }
+
+    public function setValidation(?bool $validation): static
+    {
+        $this->validation = $validation;
+
+        return $this;
+    }
+
+    public function getJoursOuverture(): ?string
+    {
+        return $this->jours_ouverture;
+    }
+
+    public function setJoursOuverture(?string $jours_ouverture): static
+    {
+        $this->jours_ouverture = $jours_ouverture;
+
+        return $this;
+    }
+
+    public function getHorrairesOuverture(): ?string
+    {
+        return $this->horraires_ouverture;
+    }
+
+    public function setHorrairesOuverture(?string $horraires_ouverture): static
+    {
+        $this->horraires_ouverture = $horraires_ouverture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getEtablissement(): Collection
+    {
+        return $this->etablissement;
+    }
+
+    public function addEtablissement(User $etablissement): static
+    {
+        if (!$this->etablissement->contains($etablissement)) {
+            $this->etablissement->add($etablissement);
+            $etablissement->setEtablissement($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEtablissement(User $etablissement): static
+    {
+        if ($this->etablissement->removeElement($etablissement)) {
+            // set the owning side to null (unless already changed)
+            if ($etablissement->getEtablissement() === $this) {
+                $etablissement->setEtablissement(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ server/src/Entity/User.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ApiResource]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $mot_de_passe = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\ManyToOne(inversedBy: 'etablissement')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?etablissement $etablissement = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->mot_de_passe;
+    }
+
+    public function setMotDePasse(string $mot_de_passe): static
+    {
+        $this->mot_de_passe = $mot_de_passe;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getEtablissement(): ?etablissement
+    {
+        return $this->etablissement;
+    }
+
+    public function setEtablissement(?etablissement $etablissement): static
+    {
+        $this->etablissement = $etablissement;
+
+        return $this;
+    }
+}
