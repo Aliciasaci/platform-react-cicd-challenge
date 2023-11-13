e296a420b7d98a52922527b43e1eefb79299ed2d
maithi-trandiep <thi.tdm04@gmail.com>
2023-11-13 02:39:09 +0100

    fix: router to add BackOfficeLayout

--- client/platform/src/App.jsx
+++ client/platform/src/App.jsx
@@ -1,23 +1,34 @@
 import {
   createBrowserRouter,
-  Route,
-  createRoutesFromElements,
   RouterProvider
 } from 'react-router-dom';
 import './App.css'
 import PrestataireRegister from './components/PrestataireRegister'
 import PlatformLayout from './layouts/PlatformLayout';
+import BackOfficeLayout from './layouts/BackofficeLayout';
 import Home from './pages/Home';
 import AdminPanel from './pages/AdminPanel';
 
+const routes = [
+  {
+    path: '/',
+    element: <PlatformLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: 'prestataire-register', element: <PrestataireRegister /> },
+    ]
+  },
+  {
+    path: '/backoffice',
+    element: <BackOfficeLayout />,
+    children: [
+      { path: 'admin-panel', element: <AdminPanel /> },
+    ]
+  },
+]     
+
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<PlatformLayout />}>
-      <Route index element={<Home />} />
-      <Route path="admin-panel" element={<AdminPanel />} />
-      <Route path="prestataire-register" element={<PrestataireRegister />} />
-    </Route>
-  )
+  routes
 )
 
 function App() {
---
+++ client/platform/src/layouts/BackofficeLayout.jsx
@@ -0,0 +1,17 @@
+import NavBarCustom from '../components/NavBarCustom';
+import SideBarCustom from '../components/SideBarCustom';
+import { Outlet } from 'react-router-dom'
+
+export default function BackOfficeLayout() {
+    return (
+        <div className="antialiased bg-gray-50 dark:bg-gray-900">
+            <NavBarCustom />
+            <SideBarCustom />
+            <main className="p-4 md:ml-64 h-auto pt-20">
+            <Outlet />
+            </main>
+                
+           
+        </div>
+    )
+}
