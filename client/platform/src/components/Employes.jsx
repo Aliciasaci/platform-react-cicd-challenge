9ae66b9a58b97a2e51429a5cd307ee4ddf6fefa6
maithi-trandiep <thi.tdm04@gmail.com>
2023-12-06 12:49:10 +0100

    add components etablissement

---
+++ client/platform/src/components/Employes.jsx
@@ -0,0 +1,12 @@
+
+import React, { useState } from "react";
+
+const Employes = () => {
+    return (
+        <div>
+            <h1>Employes</h1>
+        </div>
+    )
+}
+
+export default Employes;
