d4de0536416bf77e44d6de766d19bf09a88ad375
BahAwa <bah.awaa@gmail.com>
2024-02-01 01:21:34 +0100

    display calendar on reservation page

--- client/platform/src/components/EmployesPrestation.jsx
+++ client/platform/src/components/EmployesPrestation.jsx
@@ -1,28 +1,11 @@
-import { Label, Select } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { Label, Select } from "flowbite-react";
 
-export default function EmployesPrestation({ prestationId, handleSelect }) {
-  const [employesPrestation, setEmployesPrestation] = useState([]);
-
-  useEffect(() => {
-    const fetchEmployesPrestation = async () => {
-      try {
-        const response = await axios.get(`https://127.0.0.1:8000/api/prestations/${prestationId}`, {
-          headers: {
-            'Accept': 'application/json'
-          }
-        });
-        setEmployesPrestation(response.data.employes);
-      } catch (error) {
-        console.error('Error fetching user information:', error);
-      }
-    };
-
-    fetchEmployesPrestation();
-  }, [prestationId]);
-
+export default function EmployesPrestation({
+  employesPrestation,
+  handleSelect,
+}) {
   const sendData = (e) => {
+    e.stopPropagation();
     const selectedEmployeeId = e.target.value;
     handleSelect(selectedEmployeeId);
   };
@@ -32,11 +15,18 @@ export default function EmployesPrestation({ prestationId, handleSelect }) {
       <div className="mb-2 block">
         <Label htmlFor="employes" value="Choisir un employé" />
       </div>
-      <Select id="employes" required onChange={sendData}>
-        <option value=""> </option>
+      <Select
+        id="employes"
+        required
+        defaultValue={"Selectionnez un employé"}
+        onChange={sendData}
+      >
+        <option disabled>Selectionnez un employé</option>
         {employesPrestation.length > 0 ? (
           employesPrestation.map((employe) => (
-            <option key={employe.id} value={employe.id}>{employe.nom} {employe.prenom} </option>
+            <option key={employe.id} value={employe.id}>
+              {employe.nom} {employe.prenom}{" "}
+            </option>
           ))
         ) : (
           <option value="">Loading...</option>
