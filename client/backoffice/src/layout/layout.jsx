2f454b699e3702ea8cf0d5b25ce2e70052f7c35f
Alicia Saci <lyciiasci@gmail.com>
2024-02-13 12:44:02 +0100

    test deply

--- client/backoffice/src/layout/layout.jsx
+++ client/backoffice/src/layout/layout.jsx
@@ -1,148 +1,148 @@
-import { useEffect, useContext, useRef } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import { useEventListener, useUnmountEffect } from "primereact/hooks";
-import { classNames, DomHandler } from "primereact/utils";
-import { LayoutContext } from "./context/LayoutContext";
-import PrimeReact from "primereact/api";
-import AppSidebar from "./AppSidebar";
-import AppTopbar from "./AppTopbar";
+// import { useEffect, useContext, useRef } from "react";
+// import { useNavigate, useLocation } from "react-router-dom";
+// import { useEventListener, useUnmountEffect } from "primereact/hooks";
+// import { classNames, DomHandler } from "primereact/utils";
+// import { LayoutContext } from "./context/LayoutContext";
+// import PrimeReact from "primereact/api";
+// import AppSidebar from "./AppSidebar";
+// import AppTopbar from "./AppTopbar";
 
-const Layout = (props) => {
-  const { layoutConfig, layoutState, setLayoutState } =
-    useContext(LayoutContext);
-  const topbarRef = useRef(null);
-  const sidebarRef = useRef(null);
+// const Layout = (props) => {
+//   const { layoutConfig, layoutState, setLayoutState } =
+//     useContext(LayoutContext);
+//   const topbarRef = useRef(null);
+//   const sidebarRef = useRef(null);
 
-  const router = useNavigate();
-  const location = useLocation();
+//   const router = useNavigate();
+//   const location = useLocation();
 
-  const [bindMenuOutsideClickListener, unbindMenuOutsideClickListener] =
-    useEventListener({
-      type: "click",
-      listener: (event) => {
-        const isOutsideClicked = !(
-          sidebarRef.current.isSameNode(event.target) ||
-          sidebarRef.current.contains(event.target) ||
-          topbarRef.current.menubutton.isSameNode(event.target) ||
-          topbarRef.current.menubutton.contains(event.target)
-        );
+//   const [bindMenuOutsideClickListener, unbindMenuOutsideClickListener] =
+//     useEventListener({
+//       type: "click",
+//       listener: (event) => {
+//         const isOutsideClicked = !(
+//           sidebarRef.current.isSameNode(event.target) ||
+//           sidebarRef.current.contains(event.target) ||
+//           topbarRef.current.menubutton.isSameNode(event.target) ||
+//           topbarRef.current.menubutton.contains(event.target)
+//         );
 
-        if (isOutsideClicked) {
-          hideMenu();
-        }
-      },
-    });
+//         if (isOutsideClicked) {
+//           hideMenu();
+//         }
+//       },
+//     });
 
-  const [
-    bindProfileMenuOutsideClickListener,
-    unbindProfileMenuOutsideClickListener,
-  ] = useEventListener({
-    type: "click",
-    listener: (event) => {
-      const isOutsideClicked = !(
-        topbarRef.current.topbarmenu.isSameNode(event.target) ||
-        topbarRef.current.topbarmenu.contains(event.target) ||
-        topbarRef.current.topbarmenubutton.isSameNode(event.target) ||
-        topbarRef.current.topbarmenubutton.contains(event.target)
-      );
+//   const [
+//     bindProfileMenuOutsideClickListener,
+//     unbindProfileMenuOutsideClickListener,
+//   ] = useEventListener({
+//     type: "click",
+//     listener: (event) => {
+//       const isOutsideClicked = !(
+//         topbarRef.current.topbarmenu.isSameNode(event.target) ||
+//         topbarRef.current.topbarmenu.contains(event.target) ||
+//         topbarRef.current.topbarmenubutton.isSameNode(event.target) ||
+//         topbarRef.current.topbarmenubutton.contains(event.target)
+//       );
 
-      if (isOutsideClicked) {
-        hideProfileMenu();
-      }
-    },
-  });
+//       if (isOutsideClicked) {
+//         hideProfileMenu();
+//       }
+//     },
+//   });
 
-  const hideMenu = () => {
-    setLayoutState((prevLayoutState) => ({
-      ...prevLayoutState,
-      overlayMenuActive: false,
-      staticMenuMobileActive: false,
-      menuHoverActive: false,
-    }));
-    unbindMenuOutsideClickListener();
-    unblockBodyScroll();
-  };
+//   const hideMenu = () => {
+//     setLayoutState((prevLayoutState) => ({
+//       ...prevLayoutState,
+//       overlayMenuActive: false,
+//       staticMenuMobileActive: false,
+//       menuHoverActive: false,
+//     }));
+//     unbindMenuOutsideClickListener();
+//     unblockBodyScroll();
+//   };
 
-  const hideProfileMenu = () => {
-    setLayoutState((prevLayoutState) => ({
-      ...prevLayoutState,
-      profileSidebarVisible: false,
-    }));
-    unbindProfileMenuOutsideClickListener();
-  };
+//   const hideProfileMenu = () => {
+//     setLayoutState((prevLayoutState) => ({
+//       ...prevLayoutState,
+//       profileSidebarVisible: false,
+//     }));
+//     unbindProfileMenuOutsideClickListener();
+//   };
 
-  const blockBodyScroll = () => {
-    DomHandler.addClass("blocked-scroll");
-  };
+//   const blockBodyScroll = () => {
+//     DomHandler.addClass("blocked-scroll");
+//   };
 
-  const unblockBodyScroll = () => {
-    DomHandler.removeClass("blocked-scroll");
-  };
+//   const unblockBodyScroll = () => {
+//     DomHandler.removeClass("blocked-scroll");
+//   };
 
-  useEffect(() => {
-    if (layoutState.overlayMenuActive || layoutState.staticMenuMobileActive) {
-      bindMenuOutsideClickListener();
-    }
+//   useEffect(() => {
+//     if (layoutState.overlayMenuActive || layoutState.staticMenuMobileActive) {
+//       bindMenuOutsideClickListener();
+//     }
 
-    layoutState.staticMenuMobileActive && blockBodyScroll();
-  }, []);
+//     layoutState.staticMenuMobileActive && blockBodyScroll();
+//   }, []);
 
-  useEffect(() => {
-    if (layoutState.profileSidebarVisible) {
-      bindProfileMenuOutsideClickListener();
-    }
-  }, []);
+//   useEffect(() => {
+//     if (layoutState.profileSidebarVisible) {
+//       bindProfileMenuOutsideClickListener();
+//     }
+//   }, []);
 
-  useEffect(() => {
-    const handleRouteChange = () => {
-      hideMenu();
-      hideProfileMenu();
-    };
+//   useEffect(() => {
+//     const handleRouteChange = () => {
+//       hideMenu();
+//       hideProfileMenu();
+//     };
 
-    // Subscribe to route changes using location
-    const unsubscribe = () => router(handleRouteChange);
+//     // Subscribe to route changes using location
+//     const unsubscribe = () => router(handleRouteChange);
 
-    // Cleanup logic
-    return () => {
-      unsubscribe();
-      unbindMenuOutsideClickListener();
-      unbindProfileMenuOutsideClickListener();
-    };
-  }, []);
+//     // Cleanup logic
+//     return () => {
+//       unsubscribe();
+//       unbindMenuOutsideClickListener();
+//       unbindProfileMenuOutsideClickListener();
+//     };
+//   }, []);
 
-  PrimeReact.ripple = true;
+//   PrimeReact.ripple = true;
 
-  useUnmountEffect(() => {
-    unbindMenuOutsideClickListener();
-    unbindProfileMenuOutsideClickListener();
-  });
+//   useUnmountEffect(() => {
+//     unbindMenuOutsideClickListener();
+//     unbindProfileMenuOutsideClickListener();
+//   });
 
-  const containerClass = classNames("layout-wrapper", {
-    "layout-overlay": layoutConfig.menuMode === "overlay",
-    "layout-static": layoutConfig.menuMode === "static",
-    "layout-static-inactive":
-      layoutState.staticMenuDesktopInactive &&
-      layoutConfig.menuMode === "static",
-    "layout-overlay-active": layoutState.overlayMenuActive,
-    "layout-mobile-active": layoutState.staticMenuMobileActive,
-    "p-input-filled": layoutConfig.inputStyle === "filled",
-    "p-ripple-disabled": !layoutConfig.ripple,
-  });
+//   const containerClass = classNames("layout-wrapper", {
+//     "layout-overlay": layoutConfig.menuMode === "overlay",
+//     "layout-static": layoutConfig.menuMode === "static",
+//     "layout-static-inactive":
+//       layoutState.staticMenuDesktopInactive &&
+//       layoutConfig.menuMode === "static",
+//     "layout-overlay-active": layoutState.overlayMenuActive,
+//     "layout-mobile-active": layoutState.staticMenuMobileActive,
+//     "p-input-filled": layoutConfig.inputStyle === "filled",
+//     "p-ripple-disabled": !layoutConfig.ripple,
+//   });
 
-  return (
-    <>
-      <div className={containerClass}>
-        <AppTopbar ref={topbarRef} />
-        <div ref={sidebarRef} className="layout-sidebar">
-          <AppSidebar />
-        </div>
-        <div className="layout-main-container">
-          <div className="layout-main">{props.children}</div>
-        </div>
-        <div className="layout-mask"></div>
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className={containerClass}>
+//         <AppTopbar ref={topbarRef} />
+//         <div ref={sidebarRef} className="layout-sidebar">
+//           <AppSidebar />
+//         </div>
+//         <div className="layout-main-container">
+//           <div className="layout-main">{props.children}</div>
+//         </div>
+//         <div className="layout-mask"></div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Layout;
+// export default Layout;
