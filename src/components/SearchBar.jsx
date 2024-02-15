8648aee41fba239eb5dc4c1ee07278b06365bf3f
Alicia Saci <lyciiasci@gmail.com>
2024-02-15 10:16:29 +0100

    test deploy

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -29,7 +29,7 @@ export default function SearchBar() {
         alert(import.meta.env.VITE_SERVER_URL);
         try {
             const response = await axios.get(
-                `https://gentle-chamber-06249-b77e1a20e14f.herokuapp.com/api/filter/?prestation.titre=${searchInput}`,
+                `${import.meta.env.VITE_SERVER_URL}/filter/?prestation.titre=${searchInput}`,
                 {
                     headers: {
                         Accept: 'application/json',
@@ -46,7 +46,7 @@ export default function SearchBar() {
             } else {
                 try {
                     const response = await axios.get(
-                        `https://gentle-chamber-06249-b77e1a20e14f.herokuapp.com/api/filter/?nom=${searchInput}`,
+                        `${import.meta.env.VITE_SERVER_URL}/filter/?nom=${searchInput}`,
                         {
                             headers: {
                                 Accept: 'application/json',
@@ -63,7 +63,7 @@ export default function SearchBar() {
                     } else {
                         try {
                             const response = await axios.get(
-                                `https://gentle-chamber-06249-b77e1a20e14f.herokuapp.com/api/filter/?prestation.category=${searchInput}`,
+                                `${import.meta.env.VITE_SERVER_URL}/filter/?prestation.category=${searchInput}`,
                                 {
                                     headers: {
                                         Accept: 'application/json',
@@ -104,7 +104,7 @@ export default function SearchBar() {
                         <label htmlFor="default-search1" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
@@ -115,7 +115,7 @@ export default function SearchBar() {
                         <label htmlFor="default-search2" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
