e906976a3ddecdb193661ae185db35e1d4ba845d
Alicia Saci <lyciiasci@gmail.com>
2024-02-15 10:39:31 +0100

    update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -26,7 +26,6 @@ export default function SearchBar() {
     };
 
     const fetchFilterResults = async (searchInput) => {
-        alert(import.meta.env.VITE_SERVER_URL);
         try {
             const response = await axios.get(
                 `${import.meta.env.VITE_SERVER_URL}/filter/?prestation.titre=${searchInput}`,
@@ -104,7 +103,7 @@ export default function SearchBar() {
                         <label htmlFor="default-search1" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="" fill="none" viewBox="0 0 20 20">
+                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
@@ -115,7 +114,7 @@ export default function SearchBar() {
                         <label htmlFor="default-search2" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="" fill="none" viewBox="0 0 20 20">
+                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
