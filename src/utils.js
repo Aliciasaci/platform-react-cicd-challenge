c8e83e24390e961a63fb2cfe59cc36fa0a4b871a
vietanh2810 <vietanhc12@gmail.com>
2024-02-16 23:15:17 +0100

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-const getApiKey = () => {
+export const getApiKey = () => {
     const key = import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY;
     return key;
 };
 
-const getServerUrl = () => {
+export const getServerUrl = () => {
     const url = import.meta.env.VITE_SERVER_URL;
     return url;
 }
